0c47004467f33891ae5bf2ca8b8c1aaca5aea08d
Arildo Borges Jr <32650006+arildojr7@users.noreply.github.com>
2018-07-03 13:06:19 -0300

    Update ViewController.swift

--- SosAsilo/asiloSOS/ViewController.swift
+++ SosAsilo/asiloSOS/ViewController.swift
@@ -7,11 +7,7 @@
 //
 
 import UIKit
-<<<<<<< HEAD
-import  CoreData
-=======
 import UserNotifications
->>>>>>> dev
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UNUserNotificationCenterDelegate {
     
@@ -66,14 +62,10 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
         
-<<<<<<< HEAD
+        
         tableView.dataSource = self
         tableView.delegate = self
-        self.tableView.rowHeight = 80.0
         
-        AppDelegate.saveContext()
-=======
-<<<<<<< HEAD
         let content2 = UNMutableNotificationContent()
         content2.title = "O FRIO CHEGOU!!"
         content2.body = "O asilo Novo Lar precisa de agasalhos"
@@ -91,73 +83,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "asilosCell") as! listaGeralCell
-=======
-        tableViewGeral.dataSource = self
-        tableViewGeral.delegate = self
->>>>>>> dev
-    }
-    
-    //Criando Instituições
-    
-    
-    
-    
-    
-    
-    
-    //função nova Instituição
-    
-    func newInstituicao (nomeInst: String, enderecoInst: String, telefoneInst: String, emailInst: String, cnpjInst: String, senhaInst: String, fotoInst: UIImage ) -> Instituicao {
         
-        let instituicao = Instituicao (context: AppDelegate.persistentContainer.viewContext)
-        instituicao.nomeInst = nomeInst
-        instituicao.enderecoInst = enderecoInst
-        instituicao.telefoneInst = telefoneInst
-        instituicao.emailInst = emailInst
-        instituicao.cnpjInst = cnpjInst
-        instituicao.senhaInst = senhaInst
-        instituicao.fotoInst = UIImagePNGRepresentation(fotoInst)
-        
-        
-        return instituicao
-        
-    }
-    
-    
-    
-    
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-    
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            print(model.data.count)
-            return model.data.count
-        }
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "asilosCell") as! listaGeralCell
-            
-            
-            let asilo = model.data[indexPath.row]
-            
-            cell.view.layer.cornerRadius = 4
-            
-            cell.labelNome.text = asilo.nomeInstituicao
-            cell.labelEndereco.text = asilo.endereco
-            cell.photoAsilo.image = UIImage(named: "\(asilo.photo)")
-            
-    
-            return cell
-            
-        }
-<<<<<<< HEAD
-    
-    
-=======
->>>>>>> 971c167687c907b2ec533de4574c16fff4c0cda2
-        
->>>>>>> dev
         
         let asilo = model.data[indexPath.row]
         
