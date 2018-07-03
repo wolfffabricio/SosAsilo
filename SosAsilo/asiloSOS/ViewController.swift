969d546f8dba1fee15b12f7c176e2ca7d6fc2612
Fabrício Wolff <wolfffabricio@gmail.com>
2018-07-03 12:05:51 -0300

    Implementação de notificações

--- SosAsilo/asiloSOS/ViewController.swift
+++ SosAsilo/asiloSOS/ViewController.swift
@@ -9,60 +9,85 @@
 import UIKit
 import UserNotifications
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UNUserNotificationCenterDelegate {
+    
+    var timer = Timer()
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var TopBar: UINavigationItem!
     @IBOutlet weak var listaGeralTable: UITableView!
     
-    let model = AsiloModel.asilos() 
+    let model = AsiloModel.asilos()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge], completionHandler: {didAllow, error in})
-        
-        let content = UNMutableNotificationContent()
-        content.title = "ATENÇÃO"
-        content.body = "O asilo Padre Cacique precisa de ajuda"
-        content.badge = 1
-        
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-        let request = UNNotificationRequest(identifier: "timerDone", content: content, trigger: trigger)
-        
-        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge], completionHandler: { didAllow, error in})
         
         tableView.dataSource = self
         tableView.delegate = self
         self.tableView.rowHeight = 80.0
-    }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+//        UIApplication.shared.applicationIconBadgeNumber = 0
+        
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(appMovedToBackground), name: Notification.Name.UIApplicationWillResignActive, object: nil)
     }
     
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            print(model.data.count)
-            return model.data.count
-        }
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "asilosCell") as! listaGeralCell
-            
-            
-            let asilo = model.data[indexPath.row]
-            
-            cell.layer.cornerRadius = 8
-            
-            cell.labelNome.text = asilo.nomeInstituicao
-            cell.labelEndereco.text = asilo.endereco
-            cell.photoAsilo.image = UIImage(named: "\(asilo.photo)")
-            
+    @objc func appMovedToBackground() {
+        startTimer()
+    }
     
-            return cell
-            
-        }
+    
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 5, repeats: true, block: { (timer) in
+            self.sendNotification()
+        })
+    }
+    
+    func sendNotification() {
         
-        
-        
+        let content = UNMutableNotificationContent()
+        content.title = "ATENÇÃO"
+        content.body = "O asilo Padre Cacique precisa de ajuda"
 
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let request = UNNotificationRequest(identifier: "timerDone", content: content, trigger: trigger)
+
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+        
+        let content2 = UNMutableNotificationContent()
+        content2.title = "O FRIO CHEGOU!!"
+        content2.body = "O asilo Novo Lar precisa de agasalhos"
+        
+        let trigger2 = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+        let request2 = UNNotificationRequest(identifier: "timer", content: content2, trigger: trigger2)
+        
+        UNUserNotificationCenter.current().add(request2, withCompletionHandler: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(model.data.count)
+        return model.data.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "asilosCell") as! listaGeralCell
+        
+        
+        let asilo = model.data[indexPath.row]
+        
+        cell.layer.cornerRadius = 8
+        
+        cell.labelNome.text = asilo.nomeInstituicao
+        cell.labelEndereco.text = asilo.endereco
+        cell.photoAsilo.image = UIImage(named: "\(asilo.photo)")
+        
+        
+        return cell
+        
+    }
 }
 
 //extension ViewController: UITableViewDataSource, UITableViewDelegate {
