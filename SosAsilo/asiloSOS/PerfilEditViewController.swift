2b7b9ffcb78f177511cf7ef1bb8445adf31a43d9
Fabrício Wolff <wolfffabricio@gmail.com>
2018-07-12 16:31:14 -0300

    Implementação do get e set dos dados da tela de perfil editável para o banco.

--- SosAsilo/asiloSOS/PerfilEditViewController.swift
+++ SosAsilo/asiloSOS/PerfilEditViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import FirebaseDatabase
 import FirebaseAuth
 
-class PerfilEditViewController: UIViewController {
+class PerfilEditViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 let ref = Database.database().reference()
     
     
@@ -19,59 +19,136 @@ let ref = Database.database().reference()
     @IBOutlet weak var sliderValue: UISlider!
     @IBOutlet weak var txtelefone: UITextField!
     @IBOutlet weak var txtemail: UITextField!
+    @IBOutlet weak var slider: UISlider!
+    
+    @IBOutlet weak var imgPhoto: UIImageView!
+    
+    //@IBOutlet weak var btnGallery: UIButton!
+    
+    
+    let imagePicker = UIImagePickerController()
+    
+    
+    
+    @IBAction func btnGallery(_ sender: UIButton) {
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        //   print(info)
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imgPhoto.image = image
+            imagePicker.dismiss(animated: true, completion: nil)
+        }
+        
+    }
+    
+    var nomeFB: String? = ""
+    var emailFB: String? = ""
+    var photoFB: String? = ""
+    var indAlimentosFB: Double? = 0.0
+    var indEntretenimentoFB: Double? = 0.0
+    var indMedicamentosFB: Double? = 0.0
+    var indHigieneFB: Double? = 0.0
+    var txtSobreFB: String? = ""
+    var sobreFB: String? = ""
+    var cnpjFB: String? = ""
+    var siteFB: String? = ""
+    var telefoneFB: String? = ""
     
     
     
     
-    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        mostraDados()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let backItem = UIBarButtonItem()
+        backItem.title = "Cancelar"
+        navigationItem.backBarButtonItem = backItem
         
-        func mostraDados () {
-            print(Auth.auth().currentUser!.uid)
-            let value = Auth.auth().currentUser!
-            ref.child("asilos").child(value.uid).observeSingleEvent(of: .value, with: { (snapshot) in
-                // Get user value
-                let value = snapshot.value as? NSDictionary
-                let nomeFB = value?["nome"] as? String
-                let emailFB = value?["email"] as? String
-                let photoFB = value?["photo"] as! String
-                let indAlimentosFB = value?["indAlimentos"] as! Double
-                let indEntretenimentoFB = value?["indEntretenimento"] as! Double
-                let indMedicamentosFB = value?["indMedicamentos"] as! Double
-                let indHigieneFB = value?["indHigiene"] as! Double
-                let txtSobreFB = value?["sobre"] as? String
-                let sobreFB = value?["sobre"] as? String
-                let cnpjFB = value?["cnpj"] as? String
-                let siteFB = value?["site"] as? String
-                let telefoneFB = value?["telefone"] as? String
-                
-//                self.indAlimentacao.setProgress(indAlimentosFB, animated: true)
-//                self.indEntretenimento.setProgress(indEntretenimentoFB, animated: true)
-//                self.indMedicamentos.setProgress(indMedicamentosFB, animated: true)
-//                self.indHigiene.setProgress(indHigieneFB, animated: true)
-//                
-//                
-//                self.labelAlimentacao.text = String(Int(indAlimentosFB * 100))+"%"
-//                self.labelEntretenimento.text = String(Int(indEntretenimentoFB * 100))+"%"
-//                self.labelHigiene.text = String(Int(indHigieneFB * 100))+"%"
-//                self.labelMedicamentos.text = String(Int(indMedicamentosFB * 100))+"%"
-//                
-//                self.txtSobre.text = sobreFB
-//                self.photo.image = UIImage(named: "\(photoFB)")
-//                self.txtContato.text = "\(telefoneFB!)\n\(Auth.auth().currentUser!.email!)\n\(siteFB!)"
-//                // ...
-            }) { (error) in
-                // self.exibeAlerta()
-            }
+        self.navigationController?.navigationBar.tintColor = UIColor.white
+        
+        
+        self.navigationItem.setHidesBackButton(true, animated:true)
+        
+        imagePicker.delegate = self
+
+    }
+
+
+    @IBAction func segValueChange(_ sender: UISegmentedControl) {
+        
+        if sender.selectedSegmentIndex == 0 {
+            self.slider.value = Float(self.indAlimentosFB!)
+        }
+        else if (sender.selectedSegmentIndex == 1){
+            self.slider.value = Float(self.indEntretenimentoFB!)
+        }
+        else if (sender.selectedSegmentIndex == 2){
+            self.slider.value = Float(self.indMedicamentosFB!)
+        }
+        else if (sender.selectedSegmentIndex == 3){
+            self.slider.value = Float(self.indHigieneFB!)
+        }
+
+    }
+    
+    
+    func mostraDados () {
+        print(Auth.auth().currentUser!.uid)
+        let value = Auth.auth().currentUser!
+        ref.child("asilos").child(value.uid).observeSingleEvent(of: .value, with: { (snapshot) in
+            // Get user value
+            let value = snapshot.value as? NSDictionary
+            self.nomeFB = value?["nome"] as? String
+            self.emailFB = value?["email"] as? String
+            self.photoFB = value?["photo"] as? String
+            self.indAlimentosFB = value?["indAlimentos"] as? Double
+            self.indEntretenimentoFB = value?["indEntretenimento"] as? Double
+            self.indMedicamentosFB = value?["indMedicamentos"] as? Double
+            self.indHigieneFB = value?["indHigiene"] as? Double
+            self.txtSobreFB = value?["sobre"] as? String
+            self.sobreFB = value?["sobre"] as? String
+            self.cnpjFB = value?["cnpj"] as? String
+            self.siteFB = value?["site"] as? String
+            self.telefoneFB = value?["telefone"] as? String
+            
+            self.txtSobre.text = self.sobreFB
+            self.slider.value = Float(self.indAlimentosFB!)
             
             
+            
+            
+        }) { (error) in
+            // self.exibeAlerta()
         }
         
         
     }
-
-
-   
+    
+//    func salvarDados () {
+//
+//    }
+    
+    
+    @IBAction func btnSalvar(_ sender: UIBarButtonItem) {
+        let ref = Database.database().reference().child("asilos").child(Auth.auth().currentUser!.uid)
+        ref.updateChildValues([
+            "sobre": self.txtSobre.text,
+            "email": self.txtemail.text!,
+            "telefone": self.txtelefone.text!
+           // "indAlimentos":
+            ])
+        
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    
 }
